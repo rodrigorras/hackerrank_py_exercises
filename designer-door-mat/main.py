c2edea4631df6d6375c898a7fd4592aa7b3d99a5
Rodrigo Rios <rodrigo.rios@container-solutions.com>
2025-08-16 17:40:53 +0200

    Add main.py with solution

--- designer-door-mat/main.py
+++ designer-door-mat/main.py
@@ -1,1 +1,21 @@
-# Program for
+# Program for##### # if __name__ == '__main__':
+values = input().split()
+n = int(values[0])
+m = int(values[1])
+
+    
+#if ((N%2 != 0) and (M%2 != 0)):
+  
+#Beginning  part
+for i in range(1,n,2):       
+    # print(i)
+    print((".|." * i).center(m, "-"))
+
+#Middle
+
+print("WELCOME".center(m, "-"))
+
+
+for i in range(n-2, 0, -2):
+    # print(i)
+    print((".|." * i).center(m, "-"))
